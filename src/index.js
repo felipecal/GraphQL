2f0590a4ff29fc2fb2e63ca485b170b63197252c
Felipe <fcalldas@gmail.com>
2022-06-22 16:06:27 -0300

    🎨 improving structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import { ApolloServer } from 'apollo-server';
-import typeDefs from "../src/graphql/typeDefs";
-import resolvers from "../src/graphql/resolvers";
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+import path from 'path';
 
+const typesArray = fileLoader(path.join(__dirname, 'graphql', '**', '*.gql'));
+const typeDefs = mergeTypes(typesArray);
+
+const resolversArray = fileLoader(path.join(__dirname, 'graphql', '**', 'books.js'));
+const resolvers = mergeResolvers(resolversArray);
+
+const books = [
+  {
+    title: 'Clean Code',
+    author: 'Robert Cecil Martin',
+  },
+  {
+    title: 'The Clean Coder',
+    author: 'Robert Cecil Martin',
+  },
+]
 
 const server = new ApolloServer({
   typeDefs, resolvers
 });
 server.listen().then(({ url }) => console.log(`🔥 Server started at ${url}`))
+
+export default books;
+
